2b9d03282c217b59cbfbeb308d9ad82592d65426
Jordan ckn <peravjojo@gmail.com>
2026-01-04 19:29:00 +0200

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ export function About({ language }: AboutProps) {
   const content = {
     fr: {
       title: 'Le cabinet',
-      text: 'Alexandre Sztulman est Docteur en Droit et Membre du Barreau de Paris. Il enseigne le droit des sanctions internationales et européennes à l\'Université Paris 1 Panthéon-Sorbonne. Il est titulaire d\'un LL.M. en droit européen de l\'Université de Luxembourg, a poursuivi des études à la Higher School of Economics de Moscou, et a obtenu un Master en droit pénal international et des affaires à l\'Université Paris 1. Praticien du contentieux des sanctions européennes et du droit international des investissements, ses recherches examinent comment équilibrer les impératifs de sécurité mondiale avec la protection des libertés fondamentales.'
+      text: 'Alexandre Sztulman est Docteur en Droit et Avocat du Barreau de Paris. Il enseigne le droit des sanctions internationales et européennes à l\'Université Paris 1 Panthéon-Sorbonne. Titulaire d\'un LL.M. en droit européen de l\'Université de Luxembourg, a poursuivi des études à la Higher School of Economics de Moscou, et a obtenu un Master en droit pénal international et des affaires à l\'Université Paris 1. Praticien du contentieux des sanctions européennes et du droit international des investissements, ses recherches examinent comment équilibrer les impératifs de sécurité mondiale avec la protection des libertés fondamentales.'
     },
     en: {
       title: 'The Firm',
